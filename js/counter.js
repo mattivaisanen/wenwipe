8b8864fb526d50d58fd02a3520df3cc04eaf25ee
Matti Väisänen <matti.vaisanen@tuni.fi>
2023-02-02 23:43:36 +0200

    edited site width

--- js/counter.js
+++
@@ -1,6 +0,0 @@
-$("#biweekly")
-              .countdown("2023/02/16 19:00", function(event) {
-                $(this).text(
-                  event.strftime('~%D days %H:%M:%S')
-                );
-              });
