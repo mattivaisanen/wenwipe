fbbe86e11eae0b3006212c398286ffdd4a3e70b5
mattivaisanen <vaisanen.ma@gmail.com>
2023-02-09 22:49:24 +0200

    fixed some incorrect code

--- js/time-until-biweekly.js
+++ js/time-until-biweekly.js
@@ -1,20 +1,19 @@
 function biWeeklyCountDown() {
-    const now = moment();
-    let nextThursday = moment().day("Thursday").month(1).date(2).hour(14).minute(0).second(0);
-    if (nextThursday.isBefore(now)) {
-      nextThursday.add(2, "weeks");
-    }
-    const targetDate = moment(nextThursday).tz("America/New_York");
-    const diff = targetDate.diff(now);
-    const duration = moment.duration(diff);
-    const days = duration.days();
-    const hours = String(duration.hours()).padStart(2, "0");
-    const minutes = String(duration.minutes()).padStart(2, "0");
-    const seconds = String(duration.seconds()).padStart(2, "0");
-    document.getElementById("biweekly-countdown").innerHTML =
-     "~" + days + " days \n" + hours + ":" + minutes + ":" + seconds;
-  
-  
-      setTimeout(biWeeklyCountDown, 1000);
+  const now = moment();
+  let nextSecondThursday = moment().day("Thursday").month(0).date(12).hour(14).minute(0).second(0);
+  while (nextSecondThursday.isBefore(now)) {
+    nextSecondThursday.add(2, "weeks");
   }
-  
+  const targetDate = moment(nextSecondThursday).tz("America/New_York").hour(14).minute(0).second(0);
+  const diff = targetDate.diff(now);
+  const duration = moment.duration(diff);
+  const days = duration.days();
+  const hours = String(duration.hours()).padStart(2, "0");
+  const minutes = String(duration.minutes()).padStart(2, "0");
+  const seconds = String(duration.seconds()).padStart(2, "0");
+  document.getElementById("biweekly-countdown").innerHTML =
+   "~" + days + " days \n" + hours + ":" + minutes + ":" + seconds;
+
+
+    setTimeout(biWeeklyCountDown, 1000);
+}
--- js/time-until-weekly.js
+++ js/time-until-weekly.js
@@ -1,19 +1,21 @@
 function weeklyCountDown() {
-    const now = moment();
-    const nextThursday = moment().day("Thursday");
-    if (nextThursday.isBefore(now)) {
-      nextThursday.add(1, "week");
-    }
-    const targetDate = moment(nextThursday).tz("America/New_York").hour(14).minute(0).second(0);
-    const diff = targetDate.diff(now);
-    const duration = moment.duration(diff);
-    const days = duration.days();
-    const hours = String(duration.hours()).padStart(2, "0");
-    const minutes = String(duration.minutes()).padStart(2, "0");
-    const seconds = String(duration.seconds()).padStart(2, "0");
-    document.getElementById("weekly-countdown").innerHTML =
-    "~" + days + " days \n" + hours + ":" + minutes + ":" + seconds;
-  
-      setTimeout(weeklyCountDown, 1000);
+  const now = moment();
+  const nextThursday = moment().day("Thursday");
+  if (nextThursday.isBefore(now)) {
+    nextThursday.add(1, "week");
   }
-  
+  const targetDate = moment(nextThursday).tz("America/New_York").hour(14).minute(0).second(0);
+  const diff = targetDate.diff(now);
+  if (diff < 0) {
+    targetDate.add(1, "week");
+  }
+  const duration = moment.duration(targetDate.diff(now));
+  const days = duration.days();
+  const hours = String(duration.hours()).padStart(2, "0");
+  const minutes = String(duration.minutes()).padStart(2, "0");
+  const seconds = String(duration.seconds()).padStart(2, "0");
+  document.getElementById("weekly-countdown").innerHTML =
+  "~" + days + " days \n" + hours + ":" + minutes + ":" + seconds;
+
+  setTimeout(weeklyCountDown, 1000);
+}
