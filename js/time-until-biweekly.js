0c0b6c63e42cf3b72099893319bb2e3e86a45791
Matti Väisänen <matti.vaisanen@tuni.fi>
2023-02-14 00:53:51 +0200

    Possibly fixed biweekly

--- js/time-until-biweekly.js
+++ js/time-until-biweekly.js
@@ -1,10 +1,29 @@
 function biWeeklyCountDown() {
   const now = moment();
-  let nextSecondThursday = moment().day("Thursday").month(0).date(12).hour(14).minute(0).second(0);
-  while (nextSecondThursday.isBefore(now)) {
-    nextSecondThursday.add(2, "weeks");
+
+  //find first thursday of month.
+  let firstThursday = moment().startOf('month');
+  while (firstThursday.day() !== 4) {firstThursday.add(1, 'day');}
+
+  //find next thursdday
+  let nextThursday = moment().startOf('day');
+  while (nextThursday.day() !== 4) {nextThursday.add(1, 'day');}
+
+  if (firstThursday.isSame(nextThursday, 'day')){
+    firstThursday = targetDate}
+
+  else firstThursday.add(2, 'weeks');
+
+  if (firstThursday.isBefore(moment(), 'day')) {
+    firstThursday.add(4, 'weeks');
   }
-  const targetDate = moment(nextSecondThursday).tz("America/New_York").hour(14).minute(0).second(0);
+
+
+  //let nextSecondThursday = moment().day("Thursday").month(0).date(12).hour(14).minute(0).second(0);
+  //while (nextSecondThursday.isBefore(now)) {
+  //  nextSecondThursday.add(2, "weeks");
+  //}
+  const targetDate = moment(nextThursday).tz("America/New_York").hour(14).minute(0).second(0);
   const diff = targetDate.diff(now);
   const duration = moment.duration(diff);
   const days = duration.days();
