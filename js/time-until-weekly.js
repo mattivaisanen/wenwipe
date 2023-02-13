0184b145f54498689cda23cced9c80526f4bfe21
Matti Väisänen <matti.vaisanen@tuni.fi>
2023-02-14 01:48:34 +0200

    Fixed times jumping as string shortens

--- js/time-until-weekly.js
+++ js/time-until-weekly.js
@@ -1,25 +1,29 @@
 function weeklyCountDown() {
-  const now = moment();
-  const nextThursday = moment().day("Thursday");
-  if (nextThursday.isBefore(now)) {
-    nextThursday.add(1, "week");
-  }
-  const targetDate = moment(nextThursday).tz("America/New_York").hour(14).minute(0).second(0);
-  const diff = targetDate.diff(now);
-  if (diff < 0) {
-    targetDate.add(1, "week");
-  }
-  const duration = moment.duration(targetDate.diff(now));
-  const days = duration.days();
-  const hours = String(duration.hours()).padStart(2, "0");
-  const minutes = String(duration.minutes()).padStart(2, "0");
-  const seconds = String(duration.seconds()).padStart(2, "0");
+const now = moment();
+const nextThursday = moment().day("Thursday");
+if (nextThursday.isBefore(now)) {
+nextThursday.add(1, "week");
+}
+const targetDate = moment(nextThursday).tz("America/New_York").hour(14).minute(0).second(0);
+const diff = targetDate.diff(now);
+if (diff < 0) {
+targetDate.add(1, "week");
+}
+const duration = moment.duration(targetDate.diff(now));
+const days = duration.days();
+const hours = String(duration.hours()).padStart(2, "0");
+const minutes = String(duration.minutes()).padStart(2, "0");
+const seconds = String(duration.seconds()).padStart(2, "0");
 
-  if(days<2) document.getElementById("weekly-countdown").innerHTML = days + " day \n" + hours + ":" + minutes + ":" + seconds;
-  else document.getElementById("weekly-countdown").innerHTML = days + " days \n" + hours + ":" + minutes + ":" + seconds;
+let countdownString = "";
+if(days<2) countdownString += days + " day \n";
+else countdownString += days + " days \n";
+countdownString += hours + ":" + minutes + ":" + seconds;
 
-  localDate = moment(targetDate).local();
-  document.getElementById("weekly-countdown-date").innerHTML = moment(localDate).format('Do MMMM, HH:mm:ss');
+document.getElementById("weekly-countdown").innerHTML = countdownString;
 
-  setTimeout(weeklyCountDown, 1000);
+localDate = moment(targetDate).local();
+document.getElementById("weekly-countdown-date").innerHTML = moment(localDate).format('Do MMMM, HH:mm:ss');
+
+setTimeout(weeklyCountDown, 1000);
 }
